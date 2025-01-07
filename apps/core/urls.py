8215d265b9873a539c7188c25904605ac862d3a6
Matheus1237 <masian202207@gmail.com>
2025-01-06 23:48:09 -0300

    Melhorias

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from apps.core import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
-    path('cadastro_fornecedor', views.cadastro_fornecedor, name='cadastro_fornecedor'),
-    path('cadastro_produto', views.cadastro_produto, name='cadastro_produto'),
-    path('cadastro_contrato', views.cadastro_contrato, name='cadastro_contrato')
+    path('', views.home, name='home'),
+    path('cadastro_fornecedor/', views.cadastro_fornecedor, name='cadastro_fornecedor'),
+    path('cadastro_produto/', views.cadastro_produto, name='cadastro_produto'),
+    path('cadastro_contrato/<int:produto_id>/', views.cadastro_contrato, name='cadastro_contrato'),    
+    path('listagem_produtos/', views.listagem_produtos, name='listagem_produtos'),
+    path('detalhes_contrato/<int:contrato_id>/', views.detalhes_contrato, name='detalhes_contrato'),
+    path('listagem_contratos/', views.listagem_contratos, name='listagem_contratos'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Fornecedor, Contrato, Produto
-from .forms import FornecedorForm, ProdutoForm, ContratoForm
+from .forms import FornecedorForm, ProdutoForm
+from django.contrib import messages
 
 def home(request):
 
@@ -38,13 +39,41 @@ def cadastro_produto(request):
     
     return render(request, 'core/cadastro_produto.html', {'form': form})
 
-def cadastro_contrato(request):
-    if request.method == 'POST':
-        form = ContratoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = ContratoForm()
+def cadastro_contrato(request, produto_id):
+    produto = get_object_or_404(Produto, id=produto_id)
     
-    return render(request, 'core/cadastro_contrato.html', {'form': form})
+    if request.method == "POST":
+        quantidade = request.POST.get("quantidade")
+        if quantidade.isdigit():
+            quantidade = int(quantidade)
+            contrato = Contrato.objects.create(produto=produto, quantidade=quantidade)
+            messages.success(request, "Contrato criado com sucesso!")
+            
+            # Redireciona para os detalhes do contrato recém-criado
+            return redirect('detalhes_contrato', contrato_id=contrato.id)
+        else:
+            messages.error(request, "A quantidade deve ser um número válido.")
+    
+    return render(request, 'core/cadastro_contrato.html', {'produto': produto})
+
+def listagem_produtos(request):
+
+    produtos = Produto.objects.all()
+
+    return render(request, 'core/listagem_produtos.html', {'produtos': produtos})
+
+def detalhes_contrato(request, contrato_id):
+
+    contrato = get_object_or_404(Contrato, id=contrato_id)
+    valor_total = contrato.produto.valor_por_unidade * contrato.quantidade
+
+    return render(request, 'core/detalhes_contrato.html', {'contrato': contrato, 'valor_total': valor_total})
+
+def listagem_contratos(request):
+
+    contratos = Contrato.objects.all()
+
+    for contrato in contratos:
+        contrato.valor_total = contrato.produto.valor_por_unidade * contrato.quantidade
+
+    return render(request, 'core/listagem_contratos.html', {'contratos': contratos})
